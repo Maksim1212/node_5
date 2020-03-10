1db3ddd9c52dcef9208d6ae536b1af6bca83e78d
maksim1212 <mr.frost201611@gmail.com>
2020-03-10 09:35:22 +0200

    feat(all): init repository
    init repository

---
+++ src/components/User/router.js
@@ -0,0 +1,65 @@
+const { Router } = require('express');
+const csrf = require('csurf');
+const UserComponent = require('../User');
+
+const csrfProtection = csrf({ cookie: true });
+
+
+/**
+ * Express router to mount user related functions on.
+ * @type {Express.Router}
+ * @const
+ */
+const router = Router();
+
+/**
+ * Route serving list of users.
+ * @name /v1/users
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.get('/', csrfProtection, UserComponent.findAll);
+
+/**
+ * Route serving a user
+ * @name /v1/users/:id
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.get('/:id', csrfProtection, UserComponent.findById);
+
+/**
+ * Route serving a new user
+ * @name /v1/users
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware
+ */
+// router.post('/create', UserComponent.create);
+router.post('/', csrfProtection, UserComponent.create);
+/**
+ * Route serving a new user
+ * @name /v1/users
+ * @function
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware
+ */
+router.put('/', csrfProtection, UserComponent.updateById);
+
+/**
+ * Route serving a new user
+ * @name /v1/users
+ * @function
+ * @inner
+ * @param {string} path -Express path
+ * @param {callback} middleware - Express middleware
+ */
+router.delete('/', csrfProtection, UserComponent.deleteById);
+
+module.exports = router;
