616c713ca452309750380179a25faf20f113a76c
maksim1212 <mr.frost201611@gmail.com>
2020-03-19 23:08:20 +0200

    fix(auth): was added drop user refresh token from db after logout action
    was added drop user refresh token from db after logout action

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -58,15 +58,15 @@ async function createUser(req, res, next) {
         req.body.password = await bcrypt.hash(req.body.password, saltRounds);
         await AuthUserService.createUser(req.body);
 
-        return res.redirect('/v1/auth/login');
+        return res.redirect('/v1/auth/login/');
     } catch (error) {
         if (error instanceof ValidationError) {
             req.flash('error', error.message);
-            return res.redirect('register');
+            return res.redirect('/v1/auth/register/');
         }
         if (error.name === 'MongoError') {
             req.flash('error', { message: dbError });
-            return res.redirect('register');
+            return res.redirect('/v1/auth/register/');
         }
         return next(error);
     }
@@ -102,6 +102,8 @@ async function login(req, res, next) {
         const user = await AuthUserService.findUser(req.body.email);
         if (error || !user) {
             console.log('user not found');
+            req.flash('error', { message: dbError });
+            return res.redirect('/v1/auth/login/');
         }
         if (!error && user) {
             const reqPassword = req.body.password;
@@ -140,6 +142,8 @@ async function login(req, res, next) {
 async function logout(req, res, next) {
     try {
         console.log('logout');
+        await AuthUserService.logout(req.session.user['_id']);
+        console.log(req.session.user['_id']);
         delete req.session.user;
         return res.status(200).redirect('/v1/auth/login');
     } catch (error) {
@@ -157,6 +161,7 @@ function forbidden(req, res) {
     console.log('forbiden');
     return res.render('403.ejs');
 }
+
 module.exports = {
     register,
     createUser,
--- src/polices/isAuth.js
+++ src/polices/isAuth.js
@@ -8,22 +8,14 @@ async function isAuthJWT(req, res, next) {
     }
     let token = req.session.user.token.accessToken;
     const tokens = await getJWTTokens(req.session.user.id);
-    console.log('token:');
-    console.log(token);
     let decoded;
-    console.log('id:');
-    console.log(req.session.user['_id']);
     try {
         decoded = jwt.verify(token, process.env.JWT_Secret_KEY);
-        console.log('decoded user:');
-        console.log(decoded.user);
     } catch (error) {
         if (error.message === 'jwt expired') {
             const user = await AuthUserService.getUserByRefreshToken(req.session.user.token.refreshToken);
             req.session.user.token.accessToken = tokens.accessToken;
             token = req.session.user.token.accessToken;
-            console.log('new:');
-            console.log(token);
             decoded = jwt.verify(token, process.env.JWT_Secret_KEY);
             if (!user) {
                 return res.redirect('/v1/auth/401/');
