b659eabc030dfc81e84608859da4f124b75fb1bc
maksim1212 <mr.frost201611@gmail.com>
2020-03-25 20:37:23 +0200

    fix(user test): fix user test

--- __test__/tests/User.test.js
+++ __test__/tests/User.test.js
@@ -9,6 +9,11 @@ const testUser = {
     fullName: 'Test User',
 };
 
+const authUser = {
+    email: 'casio@gmail.com',
+    password: '123456',
+};
+
 const { expect } = chai;
 const user = '5e73896da18a9036427ca21d';
 const accessToken = jwt.sign({ user }, process.env.JWT_Secret_KEY, { expiresIn: 5 });
@@ -31,30 +36,32 @@ describe('UserComponent -> controller', () => {
                 })
                 .catch((err) => done(err));
         });
+        it('loginUser', (done) => {
+            request(server)
+                .post('/v1/auth/login')
+                // .set('Accept', 'text/html')
+                .set('Accept', 'application/json')
+                .send({ authUser })
+                .redirects(1)
+                .expect('Content-Type', /html/)
+                .expect(200)
+                .then(({ res }) => {
+                    expect(res.text).to.include(`<td>${testUser.fullName}</td>`);
+                    done();
+                })
+                .catch((err) => done(err));
+        });
+        it('loginUser', (done) => {
+            request(server)
+                .get('/v1/auth/login')
+                // .set('Accept', 'text/html')
+                .expect('Content-Type', /html/)
+                .expect(200)
+                .then(({ res }) => {
+                    expect(res.text).to.include('Sign In');
+                    done();
+                })
+                .catch((err) => done(err));
+        });
     });
 });
-
-// describe('UserComponent -> controller', () => {
-//     describe('UserComponent -> controller -> /v1/createUser', () => {
-//         it('ADD NEW USER', (done) => {
-//             request(server)
-//                 .post('/v1/users/')
-//                 // .set('Accept', 'text/html')
-//                 .set('Accept', 'application/json')
-//                 .send({ testUser })
-//                 .redirects(1)
-//                 .expect('Content-Type', /html/)
-//                 .expect(200)
-//                 // .then(({ body }) => {
-//                 //     const expectBody = expect(body);
-//                 //     expectBody.to.have.property('data');
-//                 //     done();
-//                 // })
-//                 .then(({ res }) => {
-//                     expect(res.text).to.include(`<td>${testUser.fullName}</td>`);
-//                     done();
-//                 })
-//                 .catch((err) => done(err));
-//         });
-//     });
-// });
